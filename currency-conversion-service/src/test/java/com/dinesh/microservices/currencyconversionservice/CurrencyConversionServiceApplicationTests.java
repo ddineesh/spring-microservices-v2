a8589ee9811832a8099eebfc1768c4bc7bd096f2
Dasaratharao <dinesh.dasaratharao@capgemini.com>
2021-01-15 12:11:17 -0600

    Spring micro services v2 added

---
+++ currency-conversion-service/src/test/java/com/dinesh/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
