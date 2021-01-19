85ed4390d86ff0eb0a6da436b2fc173f9b86ab94
Dinesh Dasaratha rao <ddinesh83@gmail.com>
2021-01-19 11:21:13 -0600

    Changes related to resiliency4j and code added

--- currency-exchange-service/src/main/java/com/dinesh/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/dinesh/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -30,6 +32,23 @@ public class CircuitBreakerController {
 		logger.info(" Sample Api received the call.....");
 		ResponseEntity<String> forEntity =new RestTemplate().getForEntity("http://localhost:5001:80", String.class);
 		return forEntity.getBody();
+	}
+	
+	//This Rate limiter will throttle the api calls for particular time frame
+	// For ex: 10s => 1000, 10 seconds the particular api is called to call 1000 times only
+	@GetMapping("/rate-api")
+	@RateLimiter(name="default")
+	@Bulkhead(name="default")  // This annotation to controll the concurrency service calls
+
+	public String sampleAPI_RateLimiter()
+	{
+		logger.info(" Sample rate limiter Api received the call.....");
+		/*
+		 * ResponseEntity<String> forEntity =new
+		 * RestTemplate().getForEntity("http://localhost:5001:80", String.class); return
+		 * forEntity.getBody();
+		 */
+		return "Sample Rate Limiter API";
 	}
 	
 	public String defaultResponseForFailure(Exception ex)
